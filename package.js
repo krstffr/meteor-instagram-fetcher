7580ceccbf8acbd507203ee8fc207e5692ef9255
Kristoffer Klintberg <krstffr@gmail.com>
2014-11-21 15:55:32 +0100

    Updated package meta.

--- package.js
+++ package.js
@@ -1,7 +1,8 @@
 Package.describe({
 	name: 'krstffr:instagram-fetcher',
 	version: '0.0.1',
-	summary: 'Schedulke fetching of stuff from the Instagram API.'
+	summary: 'Fetching images from the Instagram API. Very early version.',
+	git: 'https://github.com/krstffr/meteor-instagram-fetcher'
 });
 
 Package.onUse(function ( api ) {
